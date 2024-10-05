e1c7bb02253c9dc4b33afc563da516a4dae5696a
User <johnsena322@icloud.com>
2024-10-05 16:06:42 +0400

    init

---
+++ sb.py
@@ -0,0 +1,27 @@
+import asyncio
+import random
+import sys
+
+from seleniumbase import SB
+
+
+sys.argv.append("-n")
+
+
+async def main():
+    asyncio.create_task(start())
+    asyncio.create_task(start())
+
+
+async def start():
+    loop = asyncio.get_running_loop()
+    await loop.run_in_executor(None, sb_start)
+
+
+def sb_start():
+    with SB(test=True, uc=True, xvfb=True, port=str(random.randint(0, 9999))) as sb:
+        sb.uc_open_with_reconnect("https://google.com")
+        sb.wait(30)
+
+
+asyncio.run(main())
